40ba6b6e4087a12e013cf8a81f887726579012f0
Siarhei Bautrukevich <s.boltrukevich@gmail.com>
2021-11-26 10:54:35 +0400

    feat: add sendMessage method

--- src/Telegram/Application/Bot/Telegram.php
+++ src/Telegram/Application/Bot/Telegram.php
@@ -4,14 +4,19 @@ declare(strict_types=1);
 
 namespace Zorachka\Framework\Telegram\Application\Bot;
 
+use Zorachka\Framework\Telegram\Domain\Telegram\Message;
 use Zorachka\Framework\Telegram\Domain\Telegram\WebhookUrl;
 use Zorachka\Framework\Telegram\UI\Bot\Command\BotCommand;
 
 interface Telegram
 {
     /**
+     * @param Message $message
+     */
+    public function sendMessage(Message $message): void;
+
+    /**
      * @param BotCommand[] $commands
-     * @return mixed
      */
     public function setMyCommands(array $commands): void;
 
--- src/Telegram/Infrastructure/Telegram/TelegramBot.php
+++ src/Telegram/Infrastructure/Telegram/TelegramBot.php
@@ -10,12 +10,29 @@ use TelegramBot\Api\HttpException;
 use TelegramBot\Api\InvalidJsonException;
 use Zorachka\Framework\Telegram\Application\Bot\Telegram;
 use Zorachka\Framework\Telegram\Domain\Telegram\BotCommand;
+use Zorachka\Framework\Telegram\Domain\Telegram\Message;
 use Zorachka\Framework\Telegram\Domain\Telegram\WebhookUrl;
 
 final class TelegramBot implements Telegram
 {
     public function __construct(private BotApi $bot)
     {
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function sendMessage(Message $message): void
+    {
+        $this->bot->sendMessage(
+            $message->chatId()->asString(),
+            $message->text()->asString(),
+            $message->parseMode(),
+            false,
+            null,
+            null,
+            $message->isSilent(),
+        );
     }
 
     /**
